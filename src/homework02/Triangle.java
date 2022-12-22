14654510049c48f0893741e72540a51c7b71b3b2
Anastasia Gubareva <gubareva.anastasya@gmail.com>
2022-12-22 02:44:15 +0200

    all file

---
+++ src/homework02/Triangle.java
@@ -0,0 +1,9 @@
+package homework02;
+
+public class Triangle {
+    public static void main(String[] args) {
+        int cat1 = 7;
+        int cat2 = 5;
+        System.out.println("Площа трикутника = " + (cat1*cat2)/2);
+    }
+}
---
+++ src/homework02/volume.java
@@ -0,0 +1,13 @@
+package homework02;
+
+public class volume {
+    public static void main(String[] args) {
+        Integer a = 6;
+        Integer b= 9;
+        Integer c = 6;
+        int volume = a*b*c;
+        int  length = 4*(a+b+c);
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        System.out.println("Cумарна довжина всіх сторіна = " + length);
+    }
+       }

4e8a1c3d7f5d29fb8ef9cce4df179abdd1eef150
Anastasia-Gubareva <121101268+Anastasia-Gubareva@users.noreply.github.com>
2023-01-25 22:51:50 +0200

    Delete src/homework04 directory

--- src/homework04/Sin.java
+++
@@ -1,9 +0,0 @@
-package homework04;
-
-public class Sin {
-    public static void main(String args[]){
-        for(int i =0; i < 361; i = i + 10){
-            System.out.println("sin"+ " " +i);
-        }
-    }
-}
